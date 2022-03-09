45a89cb9ff8fda177712db335a5c0152d36362dc
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2022-03-09 22:22:59 +0530

    scoresGreaterThan85

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const countries = [
   "KENYA",
 ];
 
-console.log(createArrayOfArrays(countries));
+// console.log(createArrayOfArrays(countries));
 
 function createArrayOfArrays(arr) {
   return arr.map((elem) => [
@@ -29,3 +29,62 @@ function createArrayOfArrays(arr) {
 }
 
 // ##############################################################################
+
+
+/**
+ * Write a function which filter users who has scoresGreaterThan85, 
+ * Write a function which addUser to the user array only if the user does not exist.
+ * Write a function which addUserSkill which can add skill to a user only if the user exist.
+ * Write a function which editUser if the user exist in the users array.
+ */
+
+ const users = [
+    {
+        name:'Brook', 
+        scores:75,
+        skills:['HTM', 'CSS', 'JS'],
+        age:16
+    },
+    {
+        name:'Alex', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    }, 
+    {
+        name:'David', 
+        scores:75,
+        skills:['HTM', 'CSS'],
+        age:22
+    }, 
+    {
+        name:'John', 
+        scores:85,
+        skills:['HTM'],
+        age:25
+    },
+    {
+        name:'Sara',
+        scores:95,
+        skills:['HTM', 'CSS', 'JS'],
+        age: 26
+    },
+    {
+        name:'Martha', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    },
+    {
+        name:'Thomas',
+        scores:90,
+        skills:['HTM', 'CSS', 'JS'],
+        age:20
+    }
+ ];
+ 
+ console.log(scoresGreaterThan85(users));
+
+function scoresGreaterThan85(data) {
+    return data.filter(({ scores }) => scores > 85)
+}
